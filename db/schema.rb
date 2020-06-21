58b75e815c7a8dd71394a6f6b0679ba10dabbb83
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-06-21 11:15:12 +0530

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_224559) do
+ActiveRecord::Schema.define(version: 2020_05_28_160105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "collection_jokes", force: :cascade do |t|
+    t.integer "joke_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "favorite_quotes", force: :cascade do |t|
     t.integer "quote_id"
     t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jokes", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
